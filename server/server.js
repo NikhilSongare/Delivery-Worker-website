e1c602ddb91b3332445e4f939c55026d169187bf
NikhilSongare <nikhilsongare2005@email.com.com>
2026-05-22 00:34:10 +0530

    Fixed backend api test route

--- server/server.js
+++ server/server.js
@@ -19,12 +19,14 @@ const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173';
 
 const app = express();
 
+
 app.get("/api/test", (req, res) => {
   res.json({
     success: true,
     message: "Backend Connected Successfully"
   });
 });
+
 
 app.post(
   '/api/payments/webhook',
